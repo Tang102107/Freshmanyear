5030c8ac77ec9738a597d42855f6390d64092407
Tang102107 <2955133891@qq.com>
2024-02-04 21:09:09 +0800

    1.23

---
+++ 1.23源.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)
+//	{
+//	case 1:m++;
+//	case 2:n++;
+//	case 3:
+//		switch(n)
+//		{
+//		case 1:n++;
+//		case 2:m++;
+//			n++;
+//			break;
+//		}
+//	case 4:m++;
+//		break;
+//	defalt:
+//		break;
+//	}
+//	printf("m=%d\n,n=%d\n", m, n);
+int i = 1;
+while (i <= 10)
+{
+	if (i == 5)
+		i++;
+	printf("%d\n", i);
+	i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
